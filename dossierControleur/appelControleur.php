81bf06088ed3a33dc27fdd34f04d08bf0cd2486a
gregory28 <gregory.rogerie@laposte.net>
2014-04-11 13:24:08 +0200

    Update appelControleur.php

--- dossierControleur/appelControleur.php
+++ dossierControleur/appelControleur.php
@@ -1,3 +1,5 @@
 <?php
 $controleur = new MetierOenline ("127.0.0.1","root","","oenline");
+// Pour se connecter a la BDD sur oenline.eu5.org
+//$controleur = new MetierOenline ("localhost","403780","3Wexperts","403780");
 ?>
